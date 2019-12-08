778d7769b397a7f75058e623f20efea125d30638
Bernadette Davis <bsd231@nyu.edu>
2019-12-08 18:00:59 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,9 +7,11 @@ class Post < ActiveRecord::Base
 
 
   def categories_attributes=(categories_attributes)
-    categories_attributes.values.each do |categories_attribute|
-      category = Category.find_or_create_by(categories_attribute)
-      self.post_categories.build(category: category)
+    if !categories_attributes.blank?
+      categories_attributes.values.each do |categories_attribute|
+        category = Category.find_or_create_by(categories_attribute)
+        self.post_categories.build(category: category)
+      end
     end
   end
 
